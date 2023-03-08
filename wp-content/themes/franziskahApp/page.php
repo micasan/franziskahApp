fdee70f17d89f2d4c65ac4b52c1543cb68c0cfa1
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-03-08 21:38:23 +0100

    routing

--- wp-content/themes/franziskahApp/page.php
+++ wp-content/themes/franziskahApp/page.php
@@ -3,12 +3,15 @@
 <?php
 wp_head();
 ?>
- 
+
 
 <?php
 if( have_posts() ){
     while( have_posts() ){
         the_post();
+
+        get_template_part( 'template-parts/content', 'page');
+
         the_content();
     }
 }
--- wp-content/themes/franziskahApp/template-parts/content-page.php
+++ wp-content/themes/franziskahApp/template-parts/content-page.php
@@ -1,4 +1,3 @@
 <?php
 the_content();
 ?>
-
